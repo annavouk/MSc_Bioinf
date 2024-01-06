156bf6b5f4bbd37c57481f2c6fd7307977942b1f
Anna Voukouna <152087802+annavouk@users.noreply.github.com>
2024-01-06 21:54:20 +0200

    Add files via upload

---
+++ example_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+{
+char seq[1000];
+int count =0;
+
+while(scanf("%s", seq) ==1)
+	{ count++; }
+
+printf"Total count is %d\n", count);
+}
